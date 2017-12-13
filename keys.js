84f21dcbbc73f1df194c5773fece06c4d5fab3d6
Lisa Ewart <ewartle@gmail.com>
2017-12-13 17:42:54 -0500

    This commit establishes initial files

---
+++ keys.js
@@ -0,0 +1,18 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'unfOQWuDFgkNeumJYg18RQuRU',
+  consumer_secret: 'mopiz5GF4IOnezrtKONhQCOjC6YBRuP7QmiRq4blhydoerhlKf',
+  access_token_key: '940758156032004096-fgprtnEpW4In1hhxorDjgxMdNejWVtx',
+  access_token_secret: 'y2PYAHFOpUvzandMcO39dREfcnoEE58ZB7aK0yrg1OOSu',
+}
+
+module.exports = twitterKeys;
+
+
+
+
+Consumer Key (API Key)	unfOQWuDFgkNeumJYg18RQuRU
+Consumer Secret (API Secret)	mopiz5GF4IOnezrtKONhQCOjC6YBRuP7QmiRq4blhydoerhlKf
+
+
